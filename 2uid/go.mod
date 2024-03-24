f366b078c26e2ec64c26a71ae7ebd5abb1348cfa
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-24 22:16:08 +0100

    solve challenge2 (uid)

--- 2uid/go.mod
+++ 2uid/go.mod
@@ -1,5 +1,5 @@
-module maelstrom-echo
+module maelstrom-unique-ids
 
 go 1.19
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231231190402-2674df7c1076 // indirect
+require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231231190402-2674df7c1076
--- 2uid/main.go
+++ 2uid/main.go
@@ -3,11 +3,37 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
+	"time"
+	"fmt"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-func main() {
-	todo fixa så att maelstrom kanske finns i huvud dirren? och att gitignore finns på rätt plats.
 
+func main() {
+	n := maelstrom.NewNode()
+	rand.Seed(time.Now().UnixNano())
+	serv := &server{n: n}
+	n.Handle("generate", serv.generate)
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type server struct {
+	n *maelstrom.Node
+}
+
+func (s *server) generate(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	var num int64 = rand.Int63n((1 << 63) - 1)
+	var id string = fmt.Sprintf("%d%s%d", time.Now().UnixNano(), s.n.ID()[1:], num)
+	body["type"] = "generate_ok"
+	body["id"] = id
+	return s.n.Reply(msg, body)
 }

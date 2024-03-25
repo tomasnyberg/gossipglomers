492306c5e8487dcd16037647dd4e61ba18089cf7
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-25 13:58:04 +0100

    first attempt at broadcasting, communication is there but they recevie unexpected messages

--- 3broadcast/main.go
+++ 3broadcast/main.go
@@ -12,7 +12,7 @@ func main() {
 	serv := &server{n: n, seen: make([]int, 0, 100)}
 	n.Handle("broadcast", serv.receive_broadcast)
 	n.Handle("read", serv.read_broadcast)
-	n.Handle("topology", serv.topology)
+	n.Handle("topology", serv.receive_topology)
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
@@ -22,6 +22,17 @@ func main() {
 type server struct {
 	n    *maelstrom.Node
 	seen []int
+	topology map[string]map[string]struct{}
+}
+
+// TODO remove and use set
+func exists(slice []int, element int) bool {
+    for _, v := range slice {
+        if v == element {
+            return true
+        }
+    }
+    return false
 }
 
 func (s *server) receive_broadcast(msg maelstrom.Message) error {
@@ -29,7 +40,23 @@ func (s *server) receive_broadcast(msg maelstrom.Message) error {
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
-	s.seen = append(s.seen, int(body["message"].(float64)))
+	var received_int int = int(body["message"].(float64))
+	if !exists(s.seen, received_int) {
+		s.seen = append(s.seen, received_int)
+		msg_body := map[string]interface{}{
+			"message": received_int,
+			"type": "broadcast",
+		}
+		msg_body_byte, err := json.Marshal(msg_body)
+		if err != nil {
+			return err
+		}
+		// Print the msg_body_byte to stderr
+		// log.Printf("msg_body_byte: %s\n", msg_body_byte)
+		for peer := range s.topology[s.n.ID()] {
+			s.n.Send(peer, json.RawMessage(msg_body_byte))
+		}
+	}
 	delete(body, "message")
 	body["type"] = "broadcast_ok"
 	return s.n.Reply(msg, body)
@@ -45,10 +72,17 @@ func (s *server) read_broadcast(msg maelstrom.Message) error {
 	return s.n.Reply(msg, body)
 }
 
-func (s *server) topology(msg maelstrom.Message) error {
+func (s *server) receive_topology(msg maelstrom.Message) error {
 	var body map[string]any
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
+	}
+	s.topology = make(map[string]map[string]struct{})
+	for k, v := range body["topology"].(map[string]interface{}) {
+		s.topology[k] = make(map[string]struct{})
+		for _, peer := range v.([]interface{}) {
+			s.topology[k][peer.(string)] = struct{}{}
+		}
 	}
 	delete(body, "topology")
 	body["type"] = "topology_ok"

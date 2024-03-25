6a2134669d8b5a7f2f2d05a9f940f8f1c12f21d8
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-03-25 10:39:14 +0100

    solve 3a

--- 3broadcast/main.go
+++ 3broadcast/main.go
@@ -15,6 +15,8 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	serv := &server{n: n, seen: make([]int, 0, 100)}
 	n.Handle("broadcast", serv.receive_broadcast)
+	n.Handle("read", serv.read_broadcast)
+	n.Handle("topology", serv.topology)
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
@@ -27,15 +29,16 @@ type server struct {
 }
 
 func (s *server) receive_broadcast(msg maelstrom.Message) error {
-	var body map[string]any
+	var body map[string]interface{}
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
-	num, ok := body["message"].(int)
+	num, ok := body["message"].(float64)
 	if !ok {
-		return fmt.Errorf("message is not an int")
+		return fmt.Errorf("message is not an int, what we got was %v", body["message"])
 	}
-	s.seen = append(s.seen, num)
+	s.seen = append(s.seen, int(num))
+	delete(body, "message")
 	body["type"] = "broadcast_ok"
 	return s.n.Reply(msg, body)
 }
@@ -45,8 +48,8 @@ func (s *server) read_broadcast(msg maelstrom.Message) error {
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
-	body["type"] = "read_broadcast_ok"
-	body["set"] = s.seen
+	body["type"] = "read_ok"
+	body["messages"] = s.seen
 	return s.n.Reply(msg, body)
 }
 
@@ -55,6 +58,7 @@ func (s *server) topology(msg maelstrom.Message) error {
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
+	delete(body, "topology")
 	body["type"] = "topology_ok"
 	return s.n.Reply(msg, body)
 }

b57e9f056331e0c7eaa7f0b8c13025b7d22ffd07
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-05-05 08:59:53 +0200

    update run.sh to be the proper params

--- 5b_kafka-log/run.sh
+++ 5b_kafka-log/run.sh
@@ -1,1 +1,1 @@
-go install . && ../maelstrom/maelstrom test -w kafka --bin ~/go/bin/maelstrom-kafka --node-count 2 --concurrency 2n --time-limit 5 --rate 10
+go install . && ../maelstrom/maelstrom test -w kafka --bin ~/go/bin/maelstrom-kafka --node-count 2 --concurrency 2n --time-limit 20 --rate 1000
